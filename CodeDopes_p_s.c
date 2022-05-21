80eed0e84c23dad3574fea4074210ad07b55e403
MrfoxAK <95500036+MrfoxAK@users.noreply.github.com>
2022-05-21 22:16:59 +0530

    Add files via upload

---
+++ CodeDopes_p_s.c
@@ -0,0 +1,51 @@
+// LEVEL 1:-
+
+
+// #include <stdio.h>
+// int main(){
+//     for(int i = 0;1;i++){
+//         printf("%d\n",i);
+//     }
+//     return 0;
+// }
+
+
+#include <stdio.h>
+int main( )
+{
+    int x = 10, y = 3, z;
+    for(z = 0; z<x; )
+    z = z++ +y;
+    printf("%d\n", z) ;
+    return 0;
+}
+
+
+// #include <stdio.h>
+// int main(){
+//     int x = 10, y = 20, z;
+//     for(z = 0;z<y;z++){
+//         if(z==x){
+//             printf("%d\n",z);
+//         }
+//         else{
+//             break;
+//         }
+//     }
+// }
+
+
+// #include <stdio.h>
+// int main(){
+//     for(int i = 0;1;i++){
+//         printf("%d\n",i);
+//     }
+//     return 0;
+// }
+
+
+
+
+
+
+
